a82d92ca27a6c788a5c0bebae34c8cdd50dd8a30
Eric Jackson <ejackson1@arizona.edu>
2024-07-29 11:56:50 -0700

    sanitize_input cleans leading & trailing space

--- lyriquizz.py
+++ lyriquizz.py
@@ -150,8 +150,10 @@ def nottoday(now, past):
 def sanitize_input(incoming: str) -> str:
     """keep only ascii text and limited punctuation, whitespace to one"""
     allowed = re.compile(r"[^,.?!'’;:#$@\w ]", flags=re.IGNORECASE)
-    incoming = re.sub(allowed, '', unidecode(incoming))
-    incoming = re.sub(r'\s+', ' ', incoming)
+    incoming = unidecode(incoming)                       # accented chars to base char
+    incoming = re.sub(allowed, '', incoming)        # keep only the allowed chars
+    incoming = re.sub(r'\s+', ' ', incoming) # shorten multiple spaces to one
+    incoming = incoming.strip()                          # strip leading/trailing whitespace
     return incoming
 
 
